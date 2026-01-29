c29754a9e1b4fe8b88ef72195b1d7d5a847eece4
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2026-01-29 11:02:24 -0300

    fea: adding crud operations to database

---
+++ 03-spring-boot-hibernate-jpa-crud/00-starter-sql-scripts/02-student-tracker.sql
@@ -0,0 +1,37 @@
+-- MYSQL
+
+CREATE DATABASE  IF NOT EXISTS `student_tracker`;
+USE `student_tracker`;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name`varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+-- POSTGRESS
+
+-- Create database
+CREATE DATABASE student_tracker;
+
+-- Connect to the database (in psql)
+\c student_tracker;
+
+-- Drop table if exists
+DROP TABLE IF EXISTS student;
+
+-- Create table
+CREATE TABLE student (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(45),
+  last_name VARCHAR(45),
+  email VARCHAR(45)
+);
---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/test/java/com/mcartagena/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcartagena.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
